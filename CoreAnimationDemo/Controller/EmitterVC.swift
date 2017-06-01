b3f186b4c43d1ae50132b47b629e6b4a300f0eb2
Silence <lukaibolkb@163.com>
2017-06-01 17:33:37 +0800

    Add left and right heart layer

--- CoreAnimationDemo/Controller/EmitterVC.swift
+++ CoreAnimationDemo/Controller/EmitterVC.swift
@@ -17,12 +17,16 @@ class EmitterVC: UIViewController {
     private var rainLayer: CAEmitterLayer!
     
     private var centerHeartLayer: CAEmitterLayer!
+    private var leftHeartLayer: CAEmitterLayer!
+    private var rightHeartLayer: CAEmitterLayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         setupRainLayer()
         setupCenterHeartLayer()
+        setupLeftHeartLayer()
+        setupRightHeartLayer()
     }
     
     private func setupRainLayer() {
@@ -61,16 +65,57 @@ class EmitterVC: UIViewController {
         cell.birthRate = 2000
         cell.scale = 0.05
         cell.scaleSpeed = -0.02
-        cell.velocity = 30
         cell.alphaSpeed = -1
+        cell.velocity = 30
         centerHeartLayer.emitterCells = [cell]
         
         view.layer.addSublayer(centerHeartLayer)
     }
+    
+    private func setupLeftHeartLayer() {
+        leftHeartLayer = CAEmitterLayer()
+        leftHeartLayer.emitterShape = kCAEmitterLayerPoint // default value, emit orientation is right
+        leftHeartLayer.emitterMode = kCAEmitterLayerVolume // default value
+        leftHeartLayer.renderMode = kCAEmitterLayerOldestFirst
+        leftHeartLayer.emitterPosition = CGPoint(x: view.bounds.midX * 0.5, y: view.bounds.midY)
+        leftHeartLayer.birthRate = 0
+        
+        let cell = CAEmitterCell()
+        cell.contents = #imageLiteral(resourceName: "Heart").cgImage
+        cell.scale = 0.5
+        cell.lifetime = 1
+        cell.birthRate = 10 // emit duration = 0.1, only 1 cell displayed each time
+        cell.alphaSpeed = -1
+        cell.velocity = 50
+        cell.emissionLongitude = -CGFloat.pi / 2
+        leftHeartLayer.emitterCells = [cell]
+        
+        view.layer.addSublayer(leftHeartLayer)
+    }
+    
+    private func setupRightHeartLayer() {
+        rightHeartLayer = CAEmitterLayer()
+        rightHeartLayer.renderMode = kCAEmitterLayerOldestFirst
+        rightHeartLayer.emitterPosition = CGPoint(x: view.bounds.midX * 1.5, y: view.bounds.midY)
+        rightHeartLayer.birthRate = 0
+        
+        let cell = CAEmitterCell()
+        cell.contents = #imageLiteral(resourceName: "Heart").cgImage
+        cell.scale = 0.5
+        cell.lifetime = 1
+        cell.birthRate = 5
+        cell.alphaSpeed = -1
+        cell.velocity = 50
+        cell.emissionLongitude = -CGFloat.pi / 2
+        cell.emissionRange = CGFloat.pi / 4
+        rightHeartLayer.emitterCells = [cell]
+        
+        view.layer.addSublayer(rightHeartLayer)
+    }
 
     @IBAction func rainButtonClicked(_ sender: UIButton) {
         let birthRateAnimation = CABasicAnimation(keyPath: "birthRate")
-        birthRateAnimation.duration = 5
+        birthRateAnimation.duration = 3 
         if rainLayer.birthRate == 0 {
             birthRateAnimation.fromValue = 0
             birthRateAnimation.toValue = 1
@@ -93,8 +138,20 @@ class EmitterVC: UIViewController {
     }
     
     @IBAction func leftHeartButtonClicked(_ sender: UIButton) {
+        leftHeartLayer.beginTime = CACurrentMediaTime()
+        leftHeartLayer.birthRate = 1
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) { [weak self] in
+            guard let strongSelf = self else { return }
+            strongSelf.leftHeartLayer.birthRate = 0
+        }
     }
     
     @IBAction func rightHeartButtonClicked(_ sender: UIButton) {
+        rightHeartLayer.beginTime = CACurrentMediaTime()
+        rightHeartLayer.birthRate = 1
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) { [weak self] in
+            guard let strongSelf = self else { return }
+            strongSelf.rightHeartLayer.birthRate = 0
+        }
     }
 }
