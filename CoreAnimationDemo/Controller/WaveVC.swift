72181938eb79cdf05cfe3a9a6dc86fe2d0fa251b
Silence <lukaibolkb@163.com>
2017-06-02 21:04:28 +0800

    Add BoatWaveView

--- CoreAnimationDemo/Controller/WaveVC.swift
+++ CoreAnimationDemo/Controller/WaveVC.swift
@@ -10,53 +10,24 @@ import UIKit
 
 class WaveVC: UIViewController {
 
-    private var waveLayer: CAShapeLayer!
-    private var currentPhase: CGFloat = 0
-    private var waveLink: CADisplayLink!
+    private var boatWaveView: BoatWaveView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .white
         
-        waveLayer = CAShapeLayer()
-        waveLayer.frame = CGRect(x: 10, y: 200, width: view.bounds.width - 20, height: 100)
-        waveLayer.fillColor = UIColor.white.cgColor
-        waveLayer.strokeColor = UIColor.blue.cgColor
-        view.layer.addSublayer(waveLayer)
-        
-        waveLink = CADisplayLink(target: self, selector: #selector(waveLinkRefresh))
-        waveLink.isPaused = true
-        waveLink.add(to: .current, forMode: .defaultRunLoopMode)
-    }
-    
-    @objc private func waveLinkRefresh() {
-        let totalWidth: CGFloat = waveLayer.frame.width
-        let totalHeight: CGFloat = waveLayer.frame.height
-        let cycleCount: CGFloat = 3
-        UIGraphicsBeginImageContext(CGSize(width: totalWidth, height: totalHeight))
-        
-        func point(at i: Int) -> CGPoint {
-            let x = CGFloat(i)
-            let angle = x / totalWidth * (CGFloat.pi * 2 * cycleCount)
-            return CGPoint(x: x, y: sin(angle + currentPhase) * totalHeight / 2)
-        }
-        
-        let path = UIBezierPath()
-        path.move(to: point(at: 0))
-        for i in 1...Int(totalWidth) {
-            path.addLine(to: point(at: i))
-        }
-        path.stroke()
-        
-        currentPhase += 0.1
-        waveLayer.path = path.cgPath
-        
-        UIGraphicsEndImageContext()
+        boatWaveView = BoatWaveView(frame: CGRect(x: 10, y: 200, width: view.bounds.width - 20, height: 100))
+        view.addSubview(boatWaveView)
     }
 
     @IBAction func startOrStop(_ sender: UIButton) {
-        waveLink.isPaused = !waveLink.isPaused
-        sender.setTitle(waveLink.isPaused ? "Start" : "Stop", for: .normal)
+        if boatWaveView.isAnimating {
+            boatWaveView.stop()
+            sender.setTitle("Start", for: .normal)
+        } else {
+            boatWaveView.start()
+            sender.setTitle("Stop", for: .normal)
+        }
     }
 }
---
+++ CoreAnimationDemo/View/BoatWaveView.swift
@@ -0,0 +1,103 @@
+//
+//  BoatWaveView.swift
+//  CoreAnimationDemo
+//
+//  Created by Kaibo Lu on 2017/6/2.
+//  Copyright © 2017年 Kaibo Lu. All rights reserved.
+//
+
+import UIKit
+
+private let kBoatImageViewSize: CGSize = CGSize(width: 20, height: 20)
+
+class BoatWaveView: UIView {
+
+    private var waveLayer: CAShapeLayer!
+    private var currentPhase: CGFloat = 0
+    private var waveLink: CADisplayLink?
+    private var boatImageView: UIImageView!
+    
+    var isAnimating: Bool {
+        return waveLink?.isPaused == false
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    deinit {
+        waveLink?.invalidate()
+    }
+    
+    private func setup() {
+        backgroundColor = .yellow
+        waveLayer = CAShapeLayer()
+        waveLayer.frame = bounds
+        waveLayer.fillColor = UIColor.white.cgColor
+        waveLayer.strokeColor = UIColor.blue.cgColor
+        layer.addSublayer(waveLayer)
+        
+        boatImageView = UIImageView(image: #imageLiteral(resourceName: "Boat"))
+        boatImageView.frame = CGRect(origin: CGPoint(x: bounds.midX - kBoatImageViewSize.width / 2,
+                                                     y: bounds.midY - kBoatImageViewSize.height),
+                                     size: kBoatImageViewSize)
+        addSubview(boatImageView)
+    }
+    
+    @objc private func waveLinkRefresh() {
+        let totalWidth: CGFloat = bounds.width
+        let totalHeight: CGFloat = bounds.height
+        let cycleCount: CGFloat = 0.5
+        UIGraphicsBeginImageContext(CGSize(width: totalWidth, height: totalHeight))
+        
+        func point(at i: Int) -> CGPoint {
+            let x = CGFloat(i)
+            let angle = x / totalWidth * (CGFloat.pi * 2 * cycleCount)
+            return CGPoint(x: x, y: (1 - sin(angle + currentPhase)) * totalHeight / 2)
+        }
+        
+        let path = UIBezierPath()
+        path.move(to: point(at: 0))
+        for i in 1...Int(totalWidth) {
+            path.addLine(to: point(at: i))
+        }
+        path.stroke()
+        waveLayer.path = path.cgPath
+        
+        UIGraphicsEndImageContext()
+        
+        let centerX = totalWidth / 2
+        let bottomCenter = point(at: Int(centerX))
+        var transform: CGAffineTransform = .identity
+        transform = transform.translatedBy(x: 0, y: bottomCenter.y - bounds.midY)
+//        transform = transform.rotated(by: atan(cos(centerX + currentPhase)))
+        boatImageView.transform = transform
+        
+        currentPhase += 0.05
+    }
+    
+    override func didMoveToSuperview() {
+        if superview == nil {
+            waveLink?.invalidate()
+        } else {
+            waveLink = CADisplayLink(target: self, selector: #selector(waveLinkRefresh))
+            waveLink?.isPaused = true
+            waveLink?.add(to: .current, forMode: .defaultRunLoopMode)
+        }
+    }
+    
+    func start() {
+        waveLink?.isPaused = false
+    }
+    
+    func stop() {
+        waveLink?.isPaused = true
+    }
+
+}
