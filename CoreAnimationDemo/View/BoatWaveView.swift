931e73343096c32e387b56505199e90724f45e8c
Silence <lukaibolkb@163.com>
2017-06-03 15:34:50 +0800

    Update BoatWaveView

--- CoreAnimationDemo/View/BoatWaveView.swift
+++ CoreAnimationDemo/View/BoatWaveView.swift
@@ -20,7 +20,7 @@ class BoatWaveView: UIView {
      */
     var step: CGFloat = 0.05 {
         didSet {
-            assert(step != 0, "Step must != 0")
+            assert(step != 0, "Step (\(step)) must != 0")
         }
     }
     
@@ -30,14 +30,22 @@ class BoatWaveView: UIView {
      */
     var cycleCount: CGFloat = 0.5 {
         didSet {
-            assert(cycleCount >= 0, "Cycle count must >= 0")
+            assert(cycleCount >= 0, "Cycle count (\(cycleCount)) must >= 0")
         }
     }
     
+    var minWaterDepth: CGFloat = 20 {
+        didSet {
+            assert(minWaterDepth > 0, "Min water depth (\(minWaterDepth)) must > 0")
+        }
+    }
+    
+    private var boatImageView: UIImageView!
+    private var skyLayer: CALayer!
     private var waveLayer: CAShapeLayer!
+    private var underWaveLayer: CALayer!
     private var currentPhase: CGFloat = 0
     private var waveLink: CADisplayLink?
-    private var boatImageView: UIImageView!
     
     var isAnimating: Bool {
         return waveLink?.isPaused == false
@@ -61,22 +69,38 @@ class BoatWaveView: UIView {
         NotificationCenter.default.addObserver(self, selector: #selector(stop), name: Notification.Name.UIApplicationWillResignActive, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(start), name: Notification.Name.UIApplicationDidBecomeActive, object: nil)
         
-        waveLayer = CAShapeLayer()
-        waveLayer.frame = bounds
-        waveLayer.fillColor = UIColor.white.cgColor
-        waveLayer.strokeColor = UIColor.blue.cgColor
-        layer.addSublayer(waveLayer)
+        let sky = CAGradientLayer()
+        sky.frame = bounds
+        sky.colors = [UIColor(red: 0.663, green: 0.812, blue: 0.961, alpha: 1).cgColor,
+                      UIColor.white.cgColor]
+        layer.addSublayer(sky)
+        skyLayer = sky
+        
+        let underWave = CAGradientLayer()
+        underWave.frame = bounds
+        underWave.colors = [UIColor(red: 0.471, green: 0.784, blue: 1.000, alpha: 1).cgColor,
+                            UIColor(red: 0.122, green: 0.365, blue: 0.788, alpha: 1).cgColor]
+        layer.addSublayer(underWave)
+        underWaveLayer = underWave
         
         boatImageView = UIImageView(image: #imageLiteral(resourceName: "Boat"))
         boatImageView.frame = CGRect(origin: CGPoint(x: bounds.midX - kBoatImageViewSize.width / 2,
                                                      y: bounds.midY - kBoatImageViewSize.height),
                                      size: kBoatImageViewSize)
         addSubview(boatImageView)
+        
+        waveLayer = CAShapeLayer()
+        let path = UIBezierPath(rect: CGRect(x: 0, y: bounds.midY, width: bounds.width, height: bounds.height / 2))
+        waveLayer.path = path.cgPath
+        underWaveLayer.mask = waveLayer
     }
     
     @objc private func waveLinkRefresh() {
+        // Wave width
         let totalWidth: CGFloat = bounds.width
-        let totalHeight: CGFloat = bounds.height - kBoatImageViewSize.height
+        // Highest wave height - lowest wave height
+        let totalHeight: CGFloat = bounds.height - kBoatImageViewSize.height - minWaterDepth
+        assert(totalWidth > 0 && totalHeight > 0, "Total width (\(totalWidth)) and total height (\(totalHeight)) must > 0")
         
         func angleInRadians(at x: CGFloat) -> CGFloat {
             return x / totalWidth * (PI_Circle * cycleCount)
@@ -97,8 +121,12 @@ class BoatWaveView: UIView {
         for i in 1...Int(totalWidth) {
             path.addLine(to: point(at: i))
         }
-        path.stroke()
+        path.addLine(to: CGPoint(x: bounds.maxX, y: bounds.maxY))
+        path.addLine(to: CGPoint(x: 0, y: bounds.maxY))
+        path.close()
+        path.fill()
         waveLayer.path = path.cgPath
+        underWaveLayer.mask = waveLayer
         
         UIGraphicsEndImageContext()
         
@@ -118,7 +146,6 @@ class BoatWaveView: UIView {
         } else if currentPhase < PI_Circle {
             currentPhase += PI_Circle
         }
-        print(currentPhase)
     }
     
     override func didMoveToSuperview() {
